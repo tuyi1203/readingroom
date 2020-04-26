62dbf0247edbb123758ceb5b2b37a481e1bf8b55
tuyi1203 <343745799@qq.com>
2020-04-26 19:20:42 +0800

    adjust progress_morals table class

--- database/migrations/2020_04_23_172518_create_progress_morals_table.php
+++ database/migrations/2020_04_23_172518_create_progress_morals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProgressMoralTable extends Migration
+class CreateProgressMoralsTable extends Migration
 {
   /**
    * Run the migrations.
